41d7f5de8fb30a6d1ca5a72f41712ee0d5b30667
Ernst <hejpadig0021@gmail.com>
2023-06-11 23:13:50 +0600

    'v0.4'

--- blog/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('',home),
-    path('post/<int:id>',post_detail),
-    path('category/',post_category),
-    path('category/<int:id>',detail_category),
-]
